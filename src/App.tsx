ee86019294703957bff949d165577ceb46ea38f5
Felipe Brenner <felipeobrenner@hotmail.com>
2021-03-19 12:45:17 -0300

    Instalando Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
 
-function App() {
+const Title = styled.h1`
+  font-size: 64px;
+  color: #8257e6;
+`
+
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Title>Hello World</Title>
     </div>
   );
 }
-
-export default App;
