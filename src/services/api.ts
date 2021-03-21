1789484a5e53b56dd9c3d6b05a7e836e0e92baca
Felipe Brenner <felipeobrenner@hotmail.com>
2021-03-21 14:06:49 -0300

    Changed axios for netlify

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  // baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://dtmoney-felipebrenner.netlify.app/api',
 })
